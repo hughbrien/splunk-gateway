39b6f5a9bc89d190b4675c5c5dc1606b671389c4
Steve Waterworth <s@steveww.org>
2018-01-24 16:26:47 +0000

    testing for content type

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 # Test the gateway when running locally via docker-compose up
+    #-H "Content-Type: application/json" \
 
-curl -v -X POST -d @event.json http://localhost:8080/gateway
+curl \
+    -v \
+    -X POST \
+    -d @event.json \
+    http://localhost:8080/gateway
 
